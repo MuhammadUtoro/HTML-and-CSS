24feedbd8299252879127c694f63c71284e7fc14
Muhammad Utoro <dioutoro@googlemail.com>
2024-05-05 06:46:00 +0200

    into JavaScript!!

---
+++ Day 29 - 33/main.js
@@ -0,0 +1,13 @@
+let myAge = 32;
+let myName = 'Dio';
+let myHobbies = [
+    'Sports',
+    'Reading',
+    'Dota2'];
+let userJobs = {
+    title: 'Developer',
+    place: 'Krefeld',
+    salary: 50000
+};
+
+alert('My hobby is ' + myHobbies[0] + ' and my jobs is a ' + userJobs.title);
