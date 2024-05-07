dbdf2a6f3c93d2fee385d7f204f7900cbcde9eaa
Muhammad Utoro <dioutoro@googlemail.com>
2024-05-07 19:13:50 +0200

    math, string and array operations

---
+++ Day 29 - 33/operations.js
@@ -0,0 +1,19 @@
+// Math operations
+
+console.log(10+4);
+console.log(10-3)
+console.log(1*4)
+console.log(7/4)
+console.log(9%4)
+
+let result = console.log((10+3-5)*10);
+
+console.log('Muhammad'+ ' ' + 'Utoro');
+
+let userName = 'Dio';
+
+console.log(userName.length);
+console.log(userName.toUpperCase());
+
+let yourHobbies = ['Reading', 'Running'];
+console.log(yourHobbies.length);
