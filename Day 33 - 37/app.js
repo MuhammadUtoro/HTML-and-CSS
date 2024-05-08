59e4d3daf57e7470a92314d9f715e46841de3854
Muhammad Utoro <dioutoro@googlemail.com>
2024-05-08 08:14:09 +0200

    drilling and querying DOM!

---
+++ Day 33 - 37/app.js
@@ -0,0 +1,12 @@
+// document.body.children[1].children[0].href = "https://google.com";
+
+// console.log(document.body.firstChild);
+
+// console.log(document.body.firstElementChild);
+
+// console.log(document.body.children[0])
+
+// console.log(document.body.children[1])
+
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://google.com'
