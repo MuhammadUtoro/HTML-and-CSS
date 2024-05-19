1d85c663fa741a979a1b145f4708bb907063cbed
Muhammad Utoro <dioutoro@googlemail.com>
2024-05-19 16:23:17 +0200

    using nodeJS, parsing data!

---
+++ Day 47/app.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+app.use(express.urlencoded({extended: false}));
+
+//localhost:3000/currenttime
+app.get('/currenttime', function(request, response) {
+    response.send('<h1>' + new Date().toISOString() + '<h1>');
+});
+
+//localhost:3000
+app.get('/', function(request, response) {
+    response.send('<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</submit></form>')
+});
+
+
+//parsing user data
+app.post('/store-user', function(request, response) {
+    const userName = request.body.username;
+
+    const filePath = path.join(__dirname, 'data', 'users.json');
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+
+    existingUsers.push(userName);
+    fs.writeFileSync(filePath, JSON.stringify(existingUsers));
+
+    response.send('<h1>Username Stored!!</h1>');
+
+});
+
+app.get('/users', function(request, response) {
+
+    const filePath = path.join(__dirname, 'data', 'users.json');
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+
+    let responseData = '<ul>';
+
+    for (const user of existingUsers) {
+        responseData += '<li>' + user + '</li>';
+    }
+
+    responseData += '</ul>';
+
+    response.send(existingUsers);
+});
+
+app.listen(3000);
