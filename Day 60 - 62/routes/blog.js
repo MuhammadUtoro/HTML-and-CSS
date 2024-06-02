ad1a25408b48a102b30d70076eb70f0acd0bbef3
Muhammad Utoro <dioutoro@googlemail.com>
2024-06-02 08:13:31 +0200

    fetching and displaying data from database

--- Day 60 - 62/routes/blog.js
+++ Day 60 - 62/routes/blog.js
@@ -8,13 +8,33 @@ router.get('/', function(req, res) {
     res.redirect('/posts');
 });
 
-router.get('/posts', function(req, res) {
-    res.render('posts-list');
+router.get('/posts', async function(req, res) {
+    const query = `
+        SELECT posts.*, authors.name AS author_name FROM posts INNER JOIN authors ON posts.authors_id = authors.id
+    `;
+    const [posts] = await db.query(query);
+    res.render('posts-list', {posts: posts});
 });
 
 router.get('/new-post', async function(req, res) {
     const [authors] = await db.query('SELECT * FROM authors');
     res.render('create-post', {authors: authors});
+});
+
+router.get('/posts/:id', async function(req, res) {
+    const query = `
+        SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts
+        INNER JOIN authors ON posts.authors_id = authors.id
+        WHERE posts.id = ?
+    `;
+
+    const[posts] = await db.query(query, [req.params.id]);
+
+    if (!posts || posts.length === 0) {
+        res.status(404).render('404');
+    }
+
+    res.render('post-detail', { post: posts[0] });
 });
 
 router.post('/posts', async function(req, res) {
